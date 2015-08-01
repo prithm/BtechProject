b5df94dd6d4a5930398dbdcafc5a4fb10c6b030a
prithm <pritspido@gmail.com>
2015-08-02 00:20:15 +0530

    Added code for owner Post counts

--- metapaths.py
+++ metapaths.py
@@ -1,28 +1,40 @@
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup             
-
 import sys
 
-class Posts:
-	def __init__(self,_postJson):
-		self.postJson = postJson
 
 def parsePostXML(input):
-	postList = []
+	posts = []
 	tree = ET.parse(input)
-	posts = tree.getroot()
-	cnt = 0
+	rawPosts = tree.getroot()
+	for rawPost in rawPosts:
+		postJson = rawPost.attrib
+		posts.append(postJson)
+	return posts
+
+def sameOwnerPaperCount(posts, postType):
+	ownerPostCount = {}
 	for post in posts:
-		postJson = post.attrib
-		print postJson
-		postList.append(postJson)
-		cnt += 1
-		if cnt == 2:
-			break
-	return postList
+		ownerUserId = post.get('OwnerUserId')
+		postTypeId = post.get('PostTypeId')
+		if postTypeId is not None and int(postTypeId) == postType and ownerUserId is not None and ownerUserId != '-1':
+			if ownerUserId not in ownerPostCount.keys():
+				ownerPostCount[ownerUserId] = 1
+			else: 
+				ownerPostCount[ownerUserId] += 1
+	return ownerPostCount
+
+def printSameOwnerPaperCount(ownerPostCount, fileout):
+	sameOwnerPaperCountOutputFile = open(fileout,'w')
+	for owner,count in ownerPostCount.items():
+			out = owner + ' ' + str(count)
+			sameOwnerPaperCountOutputFile.write(out + '\n')
+		 	
 
 def main():
-	parsePostXML(sys.argv[1])
-
+	posts = parsePostXML(sys.argv[1])
+	ownerPostCount = sameOwnerPaperCount(posts, int(sys.argv[2]))
+	printSameOwnerPaperCount(ownerPostCount, sys.argv[3])
+	
 if __name__ == '__main__':
 	main()
