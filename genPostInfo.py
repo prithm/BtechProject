b9a2434f566fcc1edafc0ce1e7638108e1d378d7
prithm <pritspido@gmail.com>
2015-08-31 14:53:14 +0530

    Changed genPosts.py to parse line by line

--- genPostInfo.py
+++ genPostInfo.py
@@ -2,24 +2,41 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import sys
 
-#postType ownerUser postId parentId(absent in case of questions)
+#postType ownerUser postId parentId tags
 def genPostTypeOwnerIdAndSelfId(input, fileout):
-	tree = ET.parse(input)
-	rawPosts = tree.getroot()
-	postInfoOutputFile = open(fileout, 'w')
-	for rawPost in rawPosts:
-		post = rawPost.attrib
-		postId = post.get('Id')
-		ownerUserId = post.get('OwnerUserId')
-		postTypeId = post.get('PostTypeId')
-		if postTypeId is not None and postId is not None and ownerUserId is not None and ownerUserId != '-1':
-			if postTypeId == '2':
-				parentId = post.get('ParentId')
-				if parentId is not None:
-					postInfoOutputFile.write(postTypeId + ' ' + ownerUserId + ' ' + postId + ' ' + parentId + '\n')
-			elif postTypeId == '1':
-				postInfoOutputFile.write(postTypeId + ' ' + ownerUserId + ' ' + postId + '\n')
+	cnt = 0
 	
+	postInfoOutputFile = open(fileout, 'w')
+	with open(input,'r') as f:
+		for line in f: 
+			cnt += 1
+			if cnt < 3:
+				continue
+			try:
+				post = ET.fromstring(line.strip().strip('\n').strip())
+			except Exception as e:
+				continue
+
+			postId = post.get('Id')
+			ownerUserId = post.get('OwnerUserId')
+			postTypeId = post.get('PostTypeId')
+			
+			tags = post.get('Tags')
+			if tags is None:
+				tags = '<>'
+			
+			if postTypeId is not None and postId is not None and ownerUserId is not None and ownerUserId != '-1':
+				if postTypeId == '2':
+					parentId = post.get('ParentId')
+					if parentId is None:
+						parentId = '-1'
+				else:
+					parentId = '-1'
+				postInfoOutputFile.write(postTypeId + ' ' + ownerUserId + ' ' + postId + ' ' + parentId + ' ' + tags + '\n')
+
+			if cnt % 1000 == 0:
+				print cnt
+
 	postInfoOutputFile.close()
 
 
